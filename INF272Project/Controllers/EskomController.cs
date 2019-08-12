6710dc15a05c153f430d8b0ddf371b71dff742a3
Gideon Botha <46783852+BlazeAkin@users.noreply.github.com>
2019-08-12 20:09:02 +0200

    Re-upload
    Did Silo, Supplier, User CRUDS aswell as templates for Index and HomePage

---
+++ INF272Project/Controllers/EskomController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272Project.Controllers
+{
+    public class EskomController : Controller
+    {
+        // GET: Telkom
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserCRUD()
+        {
+            return View();
+        }
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult SiloCRUD()
+        {
+            return View();
+        }
+        public ActionResult SupplierCRUD()
+        {
+            return View();
+        }
+
+    }
+}
