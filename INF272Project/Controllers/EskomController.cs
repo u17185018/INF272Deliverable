b38a57b3a59723794e119bc0cf35cc4b85c55518
Zandolor <steven.j.jordaan2000@gmail.com>
2019-08-14 19:47:19 +0200

    Finished Maintenance Roster

--- INF272Project/Controllers/EskomController.cs
+++ INF272Project/Controllers/EskomController.cs
@@ -29,6 +29,24 @@ namespace INF272Project.Controllers
         {
             return View();
         }
+        
+        //Additons by - Steven
+        public ActionResult MaintenanceRoster()
+        {
+            return View();
+        }
+        public ActionResult SupplierInfo()
+        {
+            return View();
+        }
+        public ActionResult DistributionCenterCRUD()
+        {
+            return View();
+        }
+        public ActionResult PowerPlantCRUD()
+        {
+            return View();
+        }
 
     }
 }
