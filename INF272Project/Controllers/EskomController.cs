c39f59ece4a2380f310b7120da7f54a2972541ae
Barend <barendstee@gmail.com>
2019-08-15 21:46:44 +0200

    ADDED Generator views
    Added Generator view
    Added Generator CRUD

--- INF272Project/Controllers/EskomController.cs
+++ INF272Project/Controllers/EskomController.cs
@@ -54,5 +54,9 @@ namespace INF272Project.Controllers
         {
             return View();
         }
+        public ActionResult GeneratorCRUD()
+        {
+            return View();
+        }
     }
 }
